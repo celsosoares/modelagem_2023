939a6bce376a29c9a02010d936864e118005094f
celsosoares <celso.soares97c@gmail.com>
2024-02-05 23:07:30 -0300

    remove perguntas do index

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ st.title('Execução Orçamentaria do Estado de São Paulo')
 
 st.subheader('Data Warehouse da execução orçamentaria do Estado de São Paulo')
 
-st.markdown('[Pergunta 1](http://localhost:8501/pergunta1)')
-st.markdown('[Pergunta 2](http://localhost:8501/pergunta2)')
-st.markdown('[Pergunta 3](http://localhost:8501/pergunta3)')
-st.markdown('[Pergunta 4](http://localhost:8501/pergunta4)')
+# st.markdown('[Pergunta 1](http://localhost:8501/pergunta1)')
+# st.markdown('[Pergunta 2](http://localhost:8501/pergunta2)')
+# st.markdown('[Pergunta 3](http://localhost:8501/pergunta3)')
+# st.markdown('[Pergunta 4](http://localhost:8501/pergunta4)')
